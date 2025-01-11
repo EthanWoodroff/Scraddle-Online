19495f5f0716591cef0f4eefaca23fe0ee481966
Ethan Woodroff <etwoodroff@outlook.com>
2025-01-11 16:41:50 +0000

    Adding Classes
    Added the Tile and TileBag classes experimentally, as well as making the grid generation modular for the board and hand.

--- Website/generateBoard.js
+++ Website/generateBoard.js
@@ -1,23 +1,32 @@
-//generation of the board
-const board = document.getElementById("board");
-for (let i = 0; i < 3; i++){
-	const row = document.createElement("div");
-	row.setAttribute("id", "row");
-	board.appendChild(row);
-	
-	for (let j = 0; j < 3; j++) {
-		const cell = document.createElement("div");
-		cell.setAttribute("id", "cell");
-		row.appendChild(cell);
+import Tile from "./tileBag.js";
+
+//function to build a grid for the board and hand
+function BuildGrid(gridName, height, width){
+	let grid = document.getElementById(gridName);
+	for (let i = 0; i < height; i++){
+		let row = document.createElement("div");
+		row.setAttribute("id", "row");
+		grid.appendChild(row);
+		
+		for (let j = 0; j < width; j++) {
+			let cell = document.createElement("div");
+			cell.setAttribute("id", "cell");
+			row.appendChild(cell);
+		}
 	}
 }
 
+//generation of the board
+BuildGrid("board",3,3);
+
 //generation of the hand
-const hand = document.getElementById("hand");
-for (let i = 0; i < 5; i++){
-	const cell = document.createElement("div");
-	cell.setAttribute("id", "cell");
-	hand.appendChild(cell);
+BuildGrid("hand",1,5);
+
+bag = new tileBag();
+tiles = bag.GetBag();
+stringg = "";
+for(let i = 0; i < 100; i++){
+	stringg += tiles[i].GetLetter();
 }
 
-//COMMENT: turn into recursive function?
+document.getElementById(debug).innerHTML = stringg;
---
+++ Website/tile.js
@@ -0,0 +1,15 @@
+class Tile{
+	constructor(letter, value){
+		this.letter = letter;
+		this.value = value;
+	}
+	
+	GetLetter(){
+		return letter;
+	}
+	GetValue(){
+		return value;
+	}
+}
+
+export default Tile;
