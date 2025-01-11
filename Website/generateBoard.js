6344857e96e689004ab9a36ba03dbf9324504bbc
Ethan Woodroff <etwoodroff@outlook.com>
2025-01-11 00:15:36 +0000

    Board Placed
    The board and hand have both been created - dynamically as arrays of divs.

---
+++ Website/generateBoard.js
@@ -0,0 +1,18 @@
+const board = document.getElementById("board");
+for (let i = 0; i < 3; i++){
+	const row = document.createElement("div");
+	row.setAttribute("id", "row");
+	board.appendChild(row);
+	for (let j = 0; j < 3; j++) {
+		const cell = document.createElement("div");
+		cell.setAttribute("id", "cell");
+		row.appendChild(cell);
+	}
+}
+
+const hand = document.getElementById("hand");
+for (let i = 0; i < 5; i++){
+	const cell = document.createElement("div");
+	cell.setAttribute("id", "cell");
+	hand.appendChild(cell);
+}
