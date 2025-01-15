811397d510b5f9e3b41954acc4303013ff3eb092
Ethan Woodroff <etwoodroff@outlook.com>
2025-01-15 23:03:34 +0000

    Add to Board
    AddTile function added to Board so that tiles can be added to the board array. This functionality will be extended to Hand.

--- Website/modules/board.js
+++ Website/modules/board.js
@@ -4,11 +4,35 @@ class Board{
 	constructor(boardWidth, boardHeight){
 		this.boardWidth = boardWidth;
 		this.boardHeight = boardHeight;
-		this.boardArray = new Array(boardWidth,boardHeight);
+		this.boardArray = this.CreateArray(this.boardWidth,this.boardHeight);
 	}
 	
 	AddTile(tile, tileX, tileY){
-		this.boardArray[tileX,tileY] = tile;
+		console.log("Add to board");
+		this.boardArray[tileX][tileY] = tile;
+		for(let i = 0; i < this.boardHeight; i++){
+			for(let j = 0; j < this.boardWidth; j++){
+				console.log(this.boardArray[j][i]);
+			}
+		}
+	}
+
+	Delete(tileX, tileY){
+		console.log("Remove from board");
+		this.boardArray[tileX][tileY] = null;
+		for(let i = 0; i < tileX+1; i++){
+			for(let j = 0; j < tileY+1; j++){
+				console.log(this.boardArray[i][j]);
+			}
+		}
+	}
+	
+	CreateArray(width,height){
+		const newArray = new Array(height);
+		for(let i = 0; i < height; i++){
+			newArray[i] = Array.apply(null, Array(height));
+		}
+		return newArray;
 	}
 }
 
