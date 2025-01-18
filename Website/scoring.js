1127bb7c26af8f8e306dfb7568346eb8066964d4
Ethan Woodroff <etwoodroff@outlook.com>
2025-01-18 14:18:44 +0000

    Score Fully Implemented
    Total score and score for each word are now displayed on submit.

--- Website/scoring.js
+++ Website/scoring.js
@@ -1,41 +1,51 @@
 import board from "./generateBoard.js"
 import {boardWidth, boardHeight} from "./generateBoard.js";
 
-function Check(event){
-	//board.PrintBoard();
-	getScore();
-}
 
-function getScore(){
-	console.log(getWords(getRows(),boardHeight));
-	console.log(getWords(getColumns(),boardWidth));
+
+function Score(event){
+	const totalScore = document.getElementById("totalScore");
+	const scores = document.getElementById("scores");
+	const words = (getWords(getRows(),boardHeight)).concat(getWords(getColumns(),boardWidth));
+	let totalValue = 0;
+	for(let i = 0; i < words.length; i++){
+		let word = "";
+		let wordValue = 0;
+		for(let j = 0; j < words[i].wordLength; j++){
+			word += words[i].word[j].letter;
+			wordValue += words[i].word[j].value;
+		}
+		totalValue += wordValue;
+		scores.innerHTML += ("<br>" + word + ": " + wordValue);
+	}
+	totalScore.innerHTML = "Score: " + totalValue;
 }
 
 function getWords(sentences, length){
-	const words = Array();
+	const newWords = Array();
 	for(let i = 0; i < boardHeight; i++){
 		let sentence = sentences[i];
 		let currentWord = [];
 		let letterCount = 0;
-		let word = {};
+		let newWord = {};
 		for(let j = 0; j < length-1; j++){
 			if((sentence[j] != null && sentence[j+1] != null)||(j>0 && sentence[j] != null && sentence[j-1] != null)){
 				currentWord.push(sentence[j]);
 				letterCount++;
 			}
 			if (sentence[j+1] == null && currentWord[0] != undefined){
-				word = {currentWord, letterCount};
-				words.push(word);
+				newWord = {word: currentWord, wordLength: letterCount};
+				newWords.push(newWord);
 			}
 		}
 		if(sentence[length-1] != null && sentence[length-2] != null){
 			currentWord.push(sentence[length-1]);
 			letterCount++;
-			word = {currentWord, letterCount};
-			words.push(word);
+			newWord = {word: currentWord, wordLength: letterCount};
+			newWords.push(newWord);
 		}
 	}
-	return words;
+	return newWords;
 }
 
 function getRows(){	
@@ -59,4 +69,4 @@ function getColumns(){
 }
 
 const button = document.getElementById("submit");
-button.addEventListener('click', Check);
+button.addEventListener('click', Score);
