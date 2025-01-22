fa59bcc09804fc0f56c4df9aea17360dc5bd6b9c
Ethan Woodroff <etwoodroff@outlook.com>
2025-01-22 11:51:54 +0000

    Cosmetic Additions
    Added a custom drag image and website icon, and compatibility for portrait and landscape screens.

--- Website/modules/dragAndDrop.js
+++ Website/modules/dragAndDrop.js
@@ -1,12 +1,17 @@
+const dragImage = document.createElement("img");
+dragImage.src = "/images/dragImage.png";
+dragImage.setAttribute("id", "dragImage");
+
 export function allowDrop(ev) {
   ev.preventDefault();
 
   ev.stopPropagation();
   //https://developer.mozilla.org/en-US/docs/Web/API/Event/stopPropagation 19:54 20/01/2025
 }
-
 export function drag(ev) {
   ev.dataTransfer.setData("text", ev.target.id);
+  
+  ev.dataTransfer.setDragImage(dragImage,25,25);
 }
 
 export function drop(ev) {
@@ -16,6 +21,7 @@ export function drop(ev) {
     //https://developer.mozilla.org/en-US/docs/Web/API/Event/stopPropagation 19:54 20/01/2025
 	ev.preventDefault();
 	var data = ev.dataTransfer.getData("text");
+  document.getElementById(data).style.cursor = "grab";
 	ev.target.appendChild(document.getElementById(data));
 }
 //https://www.w3schools.com/html/html5_draganddrop.asp 11/01/2025 12:56
